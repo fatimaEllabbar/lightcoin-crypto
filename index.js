97c78fb082e73e7f066e2ed46dfd323070e6b1cf
vagrant <fatima.ellabbar@gmail.com>
2020-12-03 16:17:02 +0000

    updatiing lightcoin crypto

--- index.js
+++ index.js
@@ -1,29 +1,64 @@
-let balance = 500.00;
+class Transaction {
 
-class Withdrawal {
-
-  constructor(amount) {
-    this.amount = amount;
+  constructor(amount, account) {
+    this.amount  = amount;
+    this.account = account;
   }
 
   commit() {
-    balance -= this.amount;
+    if (this.value < 0 && (this.value * -1) > this.account.balance){
+      return null;
+    }
+    this.account.balance += this.value;
+    this.time = new Date();
+    // Add the transaction to the account
+    this.account.addTransaction(this);
   }
 
 }
 
+class Deposit extends Transaction {
+
+  get value() {
+    return this.amount
+  }
+
+}
+
+class Withdrawal extends Transaction {
+
+  get value() {
+    return -this.amount;
+  }
+
+}
+
+class Account {
+
+  constructor(username) {
+    this.username = username;
+    // Have the account balance start at $0 since that makes more sense.
+    this.balance = 0;
+    this.transactions = [];
+  }
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
+  }
+
+}
+const myAccount = new Account("snow-patrol");
 
 
+console.log('Starting Balance:', myAccount.balance);
 
-// DRIVER CODE BELOW
-// We use the code below to "drive" the application logic above and make sure it's working as expected
-
-t1 = new Withdrawal(50.25);
+const t1 = new Deposit(120.00, myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
+const t3 = new Deposit(40.00, myAccount);
+t3.commit();
 
-t2 = new Withdrawal(9.99);
+const t2 = new Withdrawal(40.00, myAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+console.log('Ending Balance:', myAccount.balance);
+console.log(myAccount.transactions);
+
